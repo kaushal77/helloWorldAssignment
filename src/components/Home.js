bb8805de25c9aef788953c4751a7ca87348a51f1
kaushal77 <kaushal.chitlangia97@gmail.com>
2022-09-05 04:16:51 +0530

    feature complete and design changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
-import React,{useEffect,useState,useRef, useCallback,memo} from 'react';
+import React,{useEffect,useState,useRef, useCallback} from 'react';
 import axios from 'axios';
 import Item from './Item';
+import './Item.css';
 
 
 
@@ -9,107 +10,67 @@ function Home() {
   const [skip,setSkip] = useState(0);
   const [itemData, setItemData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [currPage, setCurrPage] = useState(1);
-  const [prevPage, setPrevPage] = useState(0);
-  const [wasLastList, setWasLastList] = useState(false);
-  const [totalData, setTotalData] = useState(0);
-  const [minPageLimit, setMinPageLimit] = useState(0);
+  const [totalData, setTotalData] = useState(1);
+  
   const listInnerRef = useRef();
-
+  var limit = 10;
   
-  
-  
-  // useEffect(() => {
-  //       fetchData();
-      
-  // }, [skip])
   const fetchData = async()=>{
     setLoading(true);
 
     await axios.get(`https://dummyjson.com/products?skip=${skip}&limit=${pageNumberLimit}`
     ).then((res)=>{
-      console.log(res);
-      // if (!res.data.products.length) {
-      //   setWasLastList(true);
-      //   return;
-      // }
-      // setPrevPage(currPage);
       setTotalData(res.data.total);
       if(itemData.length <= totalData)
       {
-        setItemData([...new Set([...itemData,...res.data.products])]);
+         setItemData([...itemData,...res.data.products]);
       }
       setTimeout(()=>{
         setLoading(false);
-      },1000);
-      
-      
+      },500);
     }
   )
-  
-}
+};
   
 
   const handleScroll = ()=>{
-    // console.log("inside handlescroll");
-    let totalUserScrollHeight = window.innerHeight + Math.ceil(window.pageYOffset) -30 ;
-    // let bottomHeight = document.documentElement.offsetHeight;
-
-    // if(scrollHeight >= bottomHeight)
-    // {
-    //   setSkip(skip=>skip+5);
-    //   fetchData();
-    // }
+   
+    let totalUserScrollHeight = window.innerHeight + Math.ceil(window.pageYOffset) ;
     if (listInnerRef.current) {
-      // console.log(listInnerRef.current)
-      const { scrollTop, scrollHeight, clientHeight } = listInnerRef.current;
-      let scrollingHeight = scrollHeight -30;
-      // console.log(scrollTop, scrollHeight, clientHeight,bottomHeight,window.pageYOffset,window.innerHeight , window.screenY);
+      
+      const { scrollHeight } = listInnerRef.current;
+      let scrollingHeight = scrollHeight;
+      
       if (totalUserScrollHeight === scrollingHeight) {
-        // let updatedSkip = updatedSkip + skip + pageNumberLimit;
-        if(pageNumberLimit != 5)
-        {
-           setpageNumberLimit(prev=>prev -5);
-           console.log('limit=',pageNumberLimit)
-        }
-        
-        setSkip(prev => prev + pageNumberLimit);
-        // console.log(skip)
-        
-        
-        // fetchData();
-        // This will be triggered after hitting the last element.
-        // API call should be made here while implementing pagination.
+        setSkip(prev => prev + limit);
+        if(limit !== 5)
+        {limit = 5;}
       }
+      if(pageNumberLimit === 10 && skip === 0)
+        {
+           setpageNumberLimit(5);
+        }
     }
   }
 
   useEffect(()=>{
-    // fetchData();
     window.addEventListener('scroll',handleScroll);
-    
   },[])
 
   useEffect(()=>{
-    console.log(skip)
-      
-      fetchData();
-    
-    console.log('skip = ',skip)
+      if(skip <= totalData -1)
+      {
+        fetchData();
+      }
   },[skip])
 
   return (
-    <div style={{display: 'flex',justifyContent: 'space-evenly',flexWrap: 'wrap'}} ref={listInnerRef}  >
-      {console.log(itemData)}
+    <div className='datalist' ref={listInnerRef} >
       {(itemData) ? (itemData.map((x)=>{
-      return <Item val = {x} key={x.title}  /> })) : <p>No data found</p>
+        
+      return <Item val = {x} key={x.id +`_`+x.title}  /> })) : <p>No data found</p>
       }
       {loading && <p style={{margin:'20px'}}>loading....</p>}
-    <section>
-        {/* <img src={require('../assets/Eden_logo.PNG')} alt="" style={{width:'100px', padding:'10px 0px',margin:'5% 0px 3% 0px'}} /> */}
-        
-        
-    </section>
     </div>
   )
 }
